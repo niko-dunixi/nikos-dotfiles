1203c7ee8a0b43faa40f03c30e4ed552629e5416
Niko Dunixi [He/Him] 🦄🚀 <1402178+niko-dunixi@users.noreply.github.com>
2023-10-05 01:10:40 -0500

    basics needed for bevy

--- install-rust.sh
+++ install-rust.sh
@@ -4,3 +4,13 @@ hash rustup &>/dev/null || {
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 }
 rustup update
+sudo apt-get install -y \
+  build-essential \
+  g++ \
+  lld \
+  libasound2-dev \
+  libudev-dev \
+  libwayland-dev \
+  libx11-dev \
+  libxkbcommon-dev \
+  pkg-config
