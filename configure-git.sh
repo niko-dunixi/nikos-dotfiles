d7585d73b122012ac1cb2eceabeee70a8a15766f
Niko Dunixi [He/Him] 🦄🚀 <1402178+niko-dunixi@users.noreply.github.com>
2023-10-04 21:57:34 -0500

    updating my git config

--- configure-git.sh
+++ configure-git.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 git config --global init.defaultbranch main
-git config --global user.name 'Niko Dunixi [He/Him] 👉🏼👉🏼'
+git config --global user.name 'Niko Dunixi [He/Him] 🦄🚀'
+git config --global user.email '1402178+niko-dunixi@users.noreply.github.com'
