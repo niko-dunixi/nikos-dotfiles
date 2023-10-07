2acaf53f9d7ed79ea5b316ada15bffc0b4a61994
Niko Dunixi [He/Him] 🦄🚀 <1402178+niko-dunixi@users.noreply.github.com>
2023-10-06 22:04:11 -0500

    set vim as default editor

--- configure-git.sh
+++ configure-git.sh
@@ -3,3 +3,4 @@ set -euxo pipefail
 git config --global init.defaultbranch main
 git config --global user.name 'Niko Dunixi [He/Him] 🦄🚀'
 git config --global user.email '1402178+niko-dunixi@users.noreply.github.com'
+git config --global core.editor vim
